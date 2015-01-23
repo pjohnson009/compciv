835b6823db2d8f857efbe65cffb543b606bc356d
Peter Lobdell Johnson <peterjoh@corn24.stanford.edu>
2015-01-23 12:05:25 -0800

    updated scraper

--- homework/wh-briefings-word-scrape/html-scraper.sh
+++ homework/wh-briefings-word-scrape/html-scraper.sh
@@ -1,1 +1,1 @@
-cat data-hold/* | pup 'div#content, p text' | grep -Eow '\w{7}+' | sort | uniq -c | sort -rn | head -n 10
+cat data-hold/* | pup 'div#content text{}' | grep -Eow '\w{7}+' | sort | uniq -c | sort -rn | head -n 10
