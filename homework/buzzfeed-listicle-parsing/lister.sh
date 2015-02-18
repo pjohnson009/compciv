723cf56a4fae5134feee08e7328886605efedcfc
Peter Lobdell Johnson <peterjoh@corn23.stanford.edu>
2015-02-18 00:13:34 -0800

    Last awk command was found on Google for flipping sides of the comma

--- homework/buzzfeed-listicle-parsing/lister.sh
+++ homework/buzzfeed-listicle-parsing/lister.sh
@@ -1,13 +1,13 @@
-for x in $(seq 1 10); do
+for x in $(seq 1 12); do
 cd ~/compciv/homework/buzzfeed-listicle-parsing/data-hold/buzzfeed-archive-2014/$x
 cat *.html
-done | pup 'a attr{href}' | grep '[[:digit:]]-[[:alpha:]]' | sed -E s/....$// | grep -oE '[[:digit:]]{1,2}-[[:alpha:]]' | sed -E s/..$// | sort | uniq -c | sort -rn | grep -v '0[[:digit:]]' | grep -oE '[[:digit:]]+ [[:digit:]]+' | sed -E s/[[:space:]]/,/ | awk 'BEGIN{FS=",[ ]*"; OFS=","}
-    {
-        for (i=NF; i>0; i--) {
-            printf "%s",$i;
-            if (i>1) {
-                printf "%s", OFS;
-            }
-        }
-        printf "\n"
-    }'
+done | pup 'a attr{href}' | grep '[[:digit:]]-[[:alpha:]]' | sed -E s/....$// | grep -oE '[[:digit:]]{1,2}-[[:alpha:]]' | sed -E s/..$// | sort | uniq -c | sort -rn | grep -v '0[[:digit:]]' | grep -oE '[[:digit:]]+ [[:digit:]]+' | sed -E s/[[:space:]]/,/ | sed -E s/[[:space:]]/,/ | awk 'BEGIN{FS=",[ ]*"; OFS=","}
+{
+for (i=NF; i>0; i--) {
+printf "%s",$i;
+if (i>1) {
+printf "%s", OFS;
+}
+}
+printf "\n"
+}'
